c07cc33923c1a00f1a4bb046118eea32edfd0dca
Max Colbert <mcolbertmp@gmail.com>
2019-12-03 11:31:23 -0800

    add starter code from lab7

---
+++ docs/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"hello"},{"l":"hello.controllers"},{"l":"hello.entities"},{"l":"hello.geojson"},{"l":"hello.repositories"}]
---
+++ src/main/java/hello/EqSearch.java
@@ -0,0 +1,27 @@
+package hello;
+
+public class EqSearch {
+
+    private int distance;
+    private int minmag;
+
+    public EqSearch(){
+        // distance = 0;
+        // minmag = 0;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+    public int getMinmag() {
+        return minmag;
+    }
+
+    public void setDistance(int newVal) {
+        distance = newVal;
+    }
+    public void setMinmag(int newVal) {
+        minmag = newVal;
+    }
+
+}
