bc5c014144cd2985cb2d0707722443c8296e908b
Max Colbert <mcolbertmp@gmail.com>
2019-12-03 18:50:27 -0800

    add location search and results, add locations menu item, add tests for all 3 menu items

---
+++ src/main/java/earthquakes/searches/LocSearch.java
@@ -0,0 +1,19 @@
+package earthquakes.searches;
+
+public class LocSearch {
+
+    private String location;
+
+    public LocSearch(){
+
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String newLocation) {
+        location = newLocation;
+    }
+
+}
