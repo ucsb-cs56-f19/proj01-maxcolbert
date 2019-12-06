4360e9519a5512367a83a184c0c628089d9dc723
Max Colbert <mcolbertmp@gmail.com>
2019-12-05 20:24:34 -0800

    edit Location bean, add location display results for new fields, append form for Add button with appropriate hidden input variables

--- src/main/java/earthquakes/controllers/LocationsController.java
+++ src/main/java/earthquakes/controllers/LocationsController.java
@@ -9,6 +9,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import earthquakes.osm.PlaceCollection;
 import earthquakes.osm.Place;
